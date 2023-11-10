642b7dd0ff364e5be4c396b0db642841c2d92eee
KiwiDev808 <lucasbacela@hotmail.com>
2023-11-09 21:34:17 -0300

    feature: add login page

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <RouterLink to="/" class="header-logo">
+      <img alt="busque quadras logo" class="logo" src="@/assets/logo.png" />
+    </RouterLink>
+    <nav class="header-menu">
+      <RouterLink to="/sobre">Sobre</RouterLink>
+      <RouterLink to="/mapa">Mapa</RouterLink>
+      <RouterLink to="/login">Login</RouterLink>
+      <RouterLink to="/termos">Termos de Uso</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: start;
+  align-items: center;
+  background-color: var(--color-primary);
+  min-height: 120px;
+  padding: 0 10rem;
+}
+
+.header-logo > img {
+  width: 130px;
+}
+
+.header-menu {
+  margin: 0 auto;
+  gap: 1.75rem;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.header-menu > a {
+  background-color: #3b3b40;
+  color: #ffffff;
+  text-decoration: none;
+  min-width: 100px;
+  font-weight: 500;
+  padding: 0.5rem 1rem;
+  min-height: 40px;
+  border-radius: 4px;
+  text-align: center;
+}
+
+.header-menu > a:hover {
+  background-color: #ffffff;
+  color: #3b3b40;
+}
+
+.header-menu > :is(a:active) {
+  background-color: #dadada;
+  color: #3b3b40;
+}
+
+.header-menu > :is(a:active, a:hover) {
+  text-decoration: underline;
+}
+</style>
---
+++ src/components/PasswordInput.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  label: string
+}>()
+
+let input = ref('')
+let error = ref('')
+const validateInput = () => {
+  error.value = input.value === '' ? 'The Input field is required' : ''
+}
+</script>
+
+<template>
+  <article class="input-wrapper">
+    <section class="input-section">
+      <label>{{ label }}</label>
+      <input
+        type="password"
+        placeholder="******************"
+        autocomplete="off"
+        v-model="input"
+        @keyup="validateInput"
+        @blue="validateInput"
+      />
+    </section>
+    <section class="error-section" v-if="error">
+      {{ error }}
+    </section>
+  </article>
+</template>
+
+<style scoped>
+.input-wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+.input-section {
+  display: flex;
+  flex-direction: column;
+}
+
+label {
+  font-weight: 500;
+}
+
+.input-section > input {
+  height: 42px;
+  border-color: var(--color-border);
+  border-radius: 2px;
+}
+
+.error-section {
+  color: red;
+}
+</style>
